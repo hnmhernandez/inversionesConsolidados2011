25d81928cf288f3f81c2a1da47cb2664dd3bb330
hnmhernandez <hnmhernandez@outlook.com>
2015-01-04 16:47:27 -0430

    Ajuste del logo y barra de redes sociales ya responsivo para pantallas grandes y medianas

--- application/controllers/principal.php
+++ application/controllers/principal.php
@@ -4,10 +4,10 @@ class Principal extends CI_Controller {
 
   public function index()
   {
-    $data['titulo'] = "Inversiones Consolidados 2011 | Home";
-    $data['cuerpo'] = "Esto es una prueba";
-    $data['cabecera'] = "cabecera";
-    $data['pie'] = "pie";
+    $data['titulo'] = "";
+    $data['cuerpo'] = "";
+    $data['cabecera'] = "";
+    $data['pie'] = "";
     $this->load->view('home', $data);
   }
 }
