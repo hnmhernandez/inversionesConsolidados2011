2b7b6d549086364ef5e71d23713af30ea39f5c16
hnmhernandez <hnmhernandez@outlook.com>
2015-01-08 23:34:36 -0430

    Arreglos en el css con respeto al media query
    listo el menu de lugares para pantallas largas

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
         <!--CSS-->
         <link href="assest/css/bootstrap.min.css" rel="stylesheet">
         <link href="assest/css/consolidados.css" rel="stylesheet">
@@ -16,10 +16,10 @@
             <div id="header" style="margin-top: 20px; margin-left: 30px;">
                 <!------------------CABECERA EN MODO PANTALLA GRANDE----------->
                 <div class="row" id="header-large">
-                    <div class="col-xs-4">	<!--LOGO-->
+                    <div class="col-md-4">	<!--LOGO-->
                         <a href="#"><img id="logo" src="assest/img/logo.png"></a>
                     </div>
-                    <div class="col-xs-8">
+                    <div class="col-md-8">
                         <ul id="menuRedesSociales">
                             <li><a href="#"><img src="assest/img/icon/mercadolibre.png"></a></li>
                             <li><a href="#"><img src="assest/img/icon/facebook.png"></a></li>
@@ -41,21 +41,27 @@
                 </div>
 
                 <!------------------CABECERA EN MODO PANTALLA MEDIANAS----------->
-                <div class="row" id="header-short">
-                    <div class="col-xs-6">	<!--LOGO-->
+                <div class="row" id="header-medium">
+                    <div class="col-sm-4">	<!--LOGO-->
                         <a href="#"><img id="logo" src="assest/img/logo.png"></a>
                     </div>
-                    <div class="col-xs-6">	<!--LOGO-->
+                    <div class="col-sm-8">
                         <ul id="menuRedesSociales">
                             <li><a href="#"><img src="assest/img/icon/mercadolibre.png"></a></li>
                             <li><a href="#"><img src="assest/img/icon/facebook.png"></a></li>
                             <li><a href="#"><img src="assest/img/icon/twitter.png"></a></li>
-                        </ul>
-                        <br/>
-                        <ul id="menuRedesSociales">
                             <li><a href="#"><img src="assest/img/icon/instagram.png"></a></li>
                             <li><a href="#"><img src="assest/img/icon/youtube.png"></a></li>
                             <li><a href="#"><img src="assest/img/icon/bbm.png"></a></li>
+                        </ul>
+                        <ul id="menuLugares">
+                            <li><a href="#">Inicio</a></li>
+                            <li><a href="#">Nosotros</a></li>
+                            <li><a href="#">Mobiliario</a></li>
+                            <li><a href="#">Papelería</a></li>
+                            <li><a href="#">Prod. Limpieza</a></li>
+                            <li><a href="#">Computación</a></li>
+                            <li><a href="#">Contacto</a></li>
                         </ul>
                     </div>
                 </div>
