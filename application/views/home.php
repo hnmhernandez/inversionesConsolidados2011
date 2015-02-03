ae31eb3b7c712023b885c9b41807043744367fef
hnmhernandez <hnmhernandez@outlook.com>
2015-02-03 00:19:19 -0430

    terminado el footer para pantallas grandes
    ademas se optimizaron las imagenes del carrusel

--- application/views/home.php
+++ application/views/home.php
@@ -87,36 +87,36 @@
             <div>
                 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                     <!-- Indicators -->
-<!--                    <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-
-                    </ol>-->
+                    <!--                    <ol class="carousel-indicators">
+                                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                    
+                                        </ol>-->
 
                     <!-- Wrapper for slides -->
                     <div class="carousel-inner" role="listbox">
                         <div class="item active">
-                            <img src="assest/img/carrousel/img.jpg" alt="...">
+                            <img src="assest/img/carrousel/img1.jpg" alt="...">
                             <div class="carousel-caption">
                                 ...
                             </div>
                         </div>
                         <div class="item">
-                            <img src="assest/img/carrousel/img1HD.jpg" alt="...">
+                            <img src="assest/img/carrousel/img2.jpg" alt="...">
                             <div class="carousel-caption">
                                 ...
                             </div>
                         </div>
                         <div class="item">
-                            <img src="assest/img/carrousel/img3HD.jpg" alt="...">
+                            <img src="assest/img/carrousel/img3.jpg" alt="...">
                             <div class="carousel-caption">
                                 ...
                             </div>
                         </div>
                         <div class="item">
-                            <img src="assest/img/carrousel/img4HD.jpg" alt="...">
+                            <img src="assest/img/carrousel/img4.jpg" alt="...">
                             <div class="carousel-caption">
                                 ...
                             </div>
@@ -124,14 +124,14 @@
                     </div>
 
                     <!-- Controls -->
-<!--                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>-->
+                    <!--                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>-->
                 </div>
             </div>
             <div id="main">
@@ -142,9 +142,35 @@
                     </div>
                 </div>
             </div>
-            
+
             <div id="footer">            
-                
+                <div class="row" style="margin-right: 0px;">
+                    <div class="col-sm-4" style="padding-right: 0px;">
+                        <p id="textFooter" style="line-height: 37px; text-align: center;margin-bottom: 0px;">Copyright &copy; 2014 Inversiones consolidados 2011 C.A</p>
+                    </div>
+
+                    <div class="col-sm-5" style="padding-right: 0px;">
+                        <ol id="linksFooter" class="breadcrumb">
+                            
+                            <li><a id="aFooter" href="#">Nosotros</a></li>
+                            <li><a id="aFooter" href="#">Mobiliario</a></li>
+                            <li><a id="aFooter" href="#">Papeler&iacute;a</a></li>
+                            <li><a id="aFooter" href="#">Computaci&oacute;n</a></li>
+                            <li><a id="aFooter" href="#">Contacto</a></li>
+                        </ol>
+                    </div>
+
+                    <div class="col-sm-3" style="padding-right: 0px">
+                        <ul id="menuRedesSocialesFooter">
+                            <li><a href="#"><img src="assest/img/icon/mercadolibre_footer.png"></a></li>
+                            <li><a href="#"><img src="assest/img/icon/facebook_footer.png"></a></li>
+                            <li><a href="#"><img src="assest/img/icon/twitter_footer.png"></a></li>
+                            <li><a href="#"><img src="assest/img/icon/instagram_footer.png"></a></li>
+                            <li><a href="#"><img src="assest/img/icon/youtube_footer.png"></a></li>
+                            <li><a href="#"><img src="assest/img/icon/bbm_footer.png"></a></li>
+                        </ul>
+                    </div>
+                </div>
                 <?= $pie ?>
             </div>
         </div>
